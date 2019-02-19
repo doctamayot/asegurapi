3872ef153306eb22f7e328a6148e5c8b0dcb903c
doctamayot <doctamayot@hotmail.com>
2019-02-19 17:25:00 -0500

    first commit

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::PinsController < ApplicationController
+  before_filter :restrict_access
+  respond_to :json
+  
   def index
     render json: Pin.all.order('created_at DESC')
   end
@@ -16,4 +19,10 @@ class Api::V1::PinsController < ApplicationController
     def pin_params
       params.require(:pin).permit(:title, :image_url)
     end
+
+    def restrict_access
+  authenticate_or_request_with_http_token do |token, options|
+    ApiKey.exists?(access_token: token)
+  end
+end
 end
